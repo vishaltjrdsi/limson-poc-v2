5038d6bcdf30cde3728b612f3877933712ae0024
Vishal <vishal.torgal@jrdsi.com>
2026-07-20 12:23:08 +0530

    updated production

--- src/auth/okta/oktaConfig.js
+++ src/auth/okta/oktaConfig.js
@@ -1,9 +1,11 @@
 const oktaConfig = {
   issuer: import.meta.env.VITE_OKTA_ISSUER,
   clientId: import.meta.env.VITE_OKTA_CLIENT_ID,
-  redirectUri: import.meta.env.VITE_OKTA_REDIRECT_URI,
+  // redirectUri: import.meta.env.VITE_OKTA_REDIRECT_URI,
+  redirectUri: window.location.origin + '/login/callback',
   scopes: import.meta.env.VITE_OKTA_SCOPES.split(" "),
   baseUrl: "https://integrator-5468445.okta.com",
+  pkce: true,
 };
 
 export default oktaConfig;
